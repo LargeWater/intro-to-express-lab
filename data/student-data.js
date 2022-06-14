2557f5c4f2cb6bb3e4e7ac707b44cc5ba4817f1b
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-13 21:08:01 -0500

    complete lab

---
+++ data/student-data.js
@@ -0,0 +1,10 @@
+const students = [
+  { name: 'Alexandrea', city: 'ATL', pronouns: 'She/her'},
+  { name: 'Andy', city: 'BOS', pronouns: 'He/him'},
+  { name: 'Beryl', city: 'DUR', pronouns: 'She/her'},
+  { name: "Brian", city: 'NYC', pronouns: 'He/him'}
+]
+
+export {
+  students
+}
